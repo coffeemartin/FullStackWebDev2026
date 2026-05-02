0eafcbfb185c318f239d8dbccdf56ed9b7cf7aec
coffeemartin <coffeemartin@gmail.com>
2026-05-02 19:40:29 +0800

    added app.db in git ignore, it was not successfuly due to file path, now I changed to *.db in gitignore so the app.db is now ignored.

--- webapp/backend/app/__init__.py
+++ webapp/backend/app/__init__.py
@@ -8,4 +8,6 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+# Import routes and models after initializing app and db to avoid circular imports
+# This was mentioned in tutorial 1 May second half of the session. 
 from app import routes, models
